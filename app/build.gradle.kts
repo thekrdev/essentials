70925a679a624ff3f56d1055b70b96ea76a57b25
sameerasw <sameerasw.com@gmail.com>
2026-06-05 01:33:18 +0530

    chore: bump version code to 47 and version name to 15.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "com.sameerasw.essentials"
         minSdk = 26
         targetSdk = 37
-        versionCode = 46
-        versionName = "15.2"
+        versionCode = 47
+        versionName = "15.3"
 
         val whatsNewCounter = 2
         buildConfigField("int", "WHATS_NEW_COUNTER", whatsNewCounter.toString())
